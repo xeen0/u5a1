8dc6824fbd812aa35d1f048e7df61f2ff6eef1f8
xeen0 <sahitsrivyshnav1999@gmail.com>
2020-07-23 07:27:43 +0530

    Added facebook compoent in about page

--- src/Pages/AboutPage/AboutPage.jsx
+++ src/Pages/AboutPage/AboutPage.jsx
@@ -4,34 +4,57 @@ import Registered from "../../Images/SL 1.svg";
 import WE from "../../Images/SL 2.png";
 import Card from "react-bootstrap/Card";
 import Testimonicals from "../../Images/SL testimonials.svg";
+import Facebook from "../../Images/SL 3 facebook.svg";
+
+import { useMediaQuery } from "react-responsive";
 
 const AboutPage = (props) => {
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 605px)" });
+
   return (
     <div>
       <div className="HomeContainerT">
         <Card
-          style={{ width: "auto" }}
           onClick={() => {
             props.history.push("/u5a1/About/Registered");
           }}
         >
           <Card.Img src={Registered} />
         </Card>
-        <Card
-          style={{ width: "auto" }}
-          onClick={() => {
-            // props.history.push("/u5a1/About/Registered");
-          }}
-        >
+        <Card>
           <Card.Img src={WE} />
         </Card>
         <Card>
           <Card.Img src={Testimonicals} />
         </Card>
+        {
+          isTabletOrMobile?<>
+          <Card>
+          <Card.Img src={Facebook} />
+        </Card>
+          </>:<>
+          </>
+        }
+        
+      </div>
+      {
+          !isTabletOrMobile?<>
+          <div className="HomeContainerT">
+        <Card
+          
+        >
+          <Card.Img src={Facebook} />
+        </Card>
+        <Card style={{opacity:0}}>
+          <Card.Img src={WE} />
+        </Card>
         <Card style={{opacity:0}}>
           <Card.Img src={Testimonicals} />
         </Card>
-      </div>
+        </div>
+          </>:<>
+          </>
+        }
     </div>
   );
 };
