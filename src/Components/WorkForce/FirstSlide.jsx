c8be1e811c5794c41fec4cfe5ebd5ce757123075
xeen0 <sahitsrivyshnav1999@gmail.com>
2020-07-27 13:57:56 +0530

    popup model

--- src/Components/WorkForce/FirstSlide.jsx
+++ src/Components/WorkForce/FirstSlide.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import { useMediaQuery } from "react-responsive";
+import Modal from "react-bootstrap/Modal"
+import Button from "react-bootstrap/Button"
+
+import GD from "../../Images/PopUps/SL-1.png"
 
 import CreativeVisulization from "../../Images/SL 1 creative visualisation.svg";
 import VisualMerchandise from "../../Images/SL 2 visual merchandise.svg";
@@ -12,11 +16,46 @@ import Photoshop from "../../Images/SL 6 photoshoot.svg";
 import videoShot from "../../Images/SL 7 videoshoot.svg";
 import Portfolio from "../../Images/SL 8 portfolio.svg";
 import Makeovers from "../../Images/SL 9 makeover.svg";
-
+function MyVerticallyCenteredModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      
+      <img  src={GD} />
+      {/* <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Modal heading
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>Centered Modal</h4>
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+          consectetur ac, vestibulum at eros.
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer> */}
+    </Modal>
+  );
+}
 const FirstSlide = (props) => {
     const isTabletOrMobile = useMediaQuery({ query: "(max-width: 767px)" });
+    const [modalShow, setModalShow] = React.useState(false);
+
   return (
     <div className="container">
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+ 
       {!isTabletOrMobile ? (
         <>
           <div className="HomeContainer">
@@ -26,7 +65,7 @@ const FirstSlide = (props) => {
             <Card>
               <Card.Img src={VisualMerchandise} />
             </Card>
-            <Card>
+            <Card onClick={() => setModalShow(true)}>
               <Card.Img src={GraphicDesign} />
             </Card>
           </div>
